5b79e632c0086491c784732f36a0f925d515448d
yeabalex_ <yeabalex18@gmail.com>
2024-06-04 07:17:32 -0700

    add comments

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,7 @@ import Hobbies from "@/components/Hobbies";
 import Twitter from "@/components/Twitter";
 
 export default function About() {
+  
   interface Song {
     track: {
       name: string,
@@ -27,10 +28,12 @@ export default function About() {
     }
   }
 
+  // Define the structure for song position
   interface SongPosition {
     backward: number,
     forward: number
   }
+
 
   const [playlistInfo, setPlayListInfo] = useState<Song[]>([]);
   const [token, setToken] = useState('');
@@ -40,6 +43,7 @@ export default function About() {
   const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
   const clientSecret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
 
+  // Fetch Spotify token
   useEffect(() => {
     const getToken = async () => {
       try {
@@ -60,10 +64,13 @@ export default function About() {
 
     getToken();
 
+    // Refresh token every hour
     const intervalId = setInterval(getToken, 60 * 60 * 1000);
     return () => clearInterval(intervalId);
   }, [clientId, clientSecret]);
 
+
+  // Fetch playlist data
   useEffect(() => {
     const fetchData = async () => {
       if (!token) return;
@@ -85,6 +92,8 @@ export default function About() {
     fetchData();
   }, [token]);
 
+
+  // Handle song index from localStorage to prevent reseting when refresh
   useEffect(() => {
     const savedIndex = localStorage.getItem('currentSongIndex');
     if (savedIndex !== null) {
@@ -102,10 +111,14 @@ export default function About() {
     return () => clearInterval(intervalId);
   }, [playlistInfo.length]);
 
+
+  // Update song position
   useEffect(() => {
     setSongPosition({ backward: index, forward: index });
   }, [index]);
 
+
+  // Function to play previous song
   const prevSong = () => {
     setSongPosition((prev) => {
       const newIndex = prev.backward > 0 ? prev.backward - 1 : playlistInfo.length - 1;
@@ -115,6 +128,8 @@ export default function About() {
     });
   };
 
+
+  // Function to play next song
   const nextSong = () => {
     setSongPosition((prev) => {
       const newIndex = (prev.forward + 1) % playlistInfo.length;
@@ -124,15 +139,16 @@ export default function About() {
     });
   };
 
+
   if (!playlistInfo.length) {
     return <div>Loading...</div>;
   }
   
-  
+  // Get current song information
   const artwork: string = playlistInfo[index].track.album.images[0].url;
   const songName: string = playlistInfo[index].track.name;
   const artistsArray: string[] = playlistInfo[index].track.artists.map(artist => artist.name);
-  const preview: string = playlistInfo[index].track.preview_url
+  const preview: string = playlistInfo[index].track.preview_url;
 
   return (
     <div className="w-[100%] flex flex-col justify-center"> 
